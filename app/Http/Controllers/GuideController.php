b5a30484365d8d7e40dc4bb7adf8d0acb3b3ccad
Rihards <rihards.abolins1@gmail.com>
2025-02-23 20:27:02 +0200

    added the backbone/blueprint for how the routes will layout with the full webpage

--- app/Http/Controllers/GuideController.php
+++ app/Http/Controllers/GuideController.php
@@ -6,14 +6,4 @@ use Illuminate\Http\Request;
 
 class GuideController extends Controller
 {
-    public function email_input(){
-        return view('guide.email_input');
-    }
-
-    public function email_processing(Request $request){ // need to add this to a model then database
-        $request->validate([
-            'email' => 'required|email'
-        ]);
-        return view('guide.thanks_for_registering');
-    }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,29 @@ use App\Http\Controllers\BlogController;
 use App\Http\Controllers\GuideController;
 use App\Http\Controllers\StripeDonationsController;
 
+# Implement proper routes for the real website.
+
+# the free guide / checklist / overview
+Route::group(["prefix"=> "guide"], function () {
+    # Route::get("/", [GuideController::class, 'main'])->name('guide.main');
+    # all the sub-routes for the guide 2.1 - 2.5
+});
+
+# route for the selling page - simple
+
+# route for the login and registration- controller
+
+# the authenticated portion, need to determine necessary middleware
+Route::group(["prefix" => "dashboard", "middleware" => ["auth",""]], function () {
+    
+    # route for the user dashboard
+
+    # all the sub-routes for the user dashboard
+
+});
+
+# Old routes
+
 Route::get("lang/{lang}", function($lang){
     if (in_array($lang, ['en', 'lv'])) {
         app()->setLocale($lang);
