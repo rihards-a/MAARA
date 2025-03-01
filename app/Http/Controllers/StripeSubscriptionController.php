3c9c1606c75cc5b57d72554ca46cab050e1ed681
Rihards <rihards.abolins1@gmail.com>
2025-03-01 13:36:09 +0200

    implemented lifetime_subscription for users, after the webhook confirming the purchase is received, the user's lifetime column gets set to true

--- app/Http/Controllers/StripeSubscriptionController.php
+++ app/Http/Controllers/StripeSubscriptionController.php
@@ -16,6 +16,11 @@ class StripeSubscriptionController extends Controller
         return $request->user()->checkout([$price_id => 1], [
             'success_url' => route('subscription.lifetime.success')."?session_id={CHECKOUT_SESSION_ID}",
             'cancel_url' => route('subscription.lifetime.cancel'),
+            'metadata' => [
+                'user_id' => $request->user()->id,
+                'is_lifetime' => true, 
+                'subscription_name' => 'LifetimeBasic',
+        ],
         ]);
     }
 
@@ -25,39 +30,33 @@ class StripeSubscriptionController extends Controller
     }
  
     public function webhook(Request $request) {
-        // This is your Stripe CLI webhook secret for testing your endpoint locally.
         $endpoint_secret = env('STRIPE_WEBHOOK_SECRET');
-
-        $payload = @file_get_contents('php://input');
-        $sig_header = $_SERVER['HTTP_STRIPE_SIGNATURE'];
+        $payload = @file_get_contents('php://input'); # safer to wrap in try catch for logging
+        $sig_header = $_SERVER['HTTP_STRIPE_SIGNATURE'] ?? ''; # if this is not set, should log, because could be a fake attempt
         $event = null;
 
-        try {
-            $event = \Stripe\Webhook::constructEvent(
-                $payload, $sig_header, $endpoint_secret
-            );
-        } catch (\UnexpectedValueException $e) {
-            // Invalid payload
-            return response('', 400);
-        } catch (\Stripe\Exception\SignatureVerificationException $e) {
-            // Invalid signature
-            return response('', 400);
-        }
+        try {$event = \Stripe\Webhook::constructEvent($payload, $sig_header, $endpoint_secret);} 
+        catch (\UnexpectedValueException $e) {return response('', 400);} // Invalid payload, log?
+        catch (\Stripe\Exception\SignatureVerificationException $e) {return response('', 400);} // Invalid signature, log?
 
-// Handle the event
+        // Handle the event
         switch ($event->type) {
             case 'checkout.session.completed':
-                $session = $event->data->object;
+                $session = $event->data->object; # this is a StripeEvent object, essentially a session, because of how much data it has
+                $metadata = $session->metadata; 
+                $user = User::find($metadata->user_id);
+                if (!$user) {return response('', 404);} // user not found
 
-                # TODO: IMPLEMENT ACTUAL LOGIC FOR HOW TO LOG THE NEW SUBSCRIPTION IN TO THE DATABASE
-                $user = User::find(2);
-                $user->google_id = "thisworks";
-                $user->save();
+                if ($metadata->is_lifetime) {
+                    $user->lifetime_subscription = true;
+                    $user->save();
+                }
+                // LOG this event
+                break;
             // ... handle other event types
             default:
-                echo 'Received unknown event type ' . $event->type;
+                # echo 'Received unknown event type ' . $event->type; // echo is not a secure way to log
         }
-
         return response('');
     }
 
