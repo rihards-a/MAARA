c9b5953183484aec4733b8c5c1d1d1bff687ce10
Rihards <rihards.abolins1@gmail.com>
2025-04-04 14:19:05 +0300

    Created the foundational migrations and started models

---
+++ database/migrations/2025_04_04_105637_create_user_questionnaire_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_questionnaire', function (Blueprint $table) {
+            $table->id();
+            // Submission data:
+            $table->timestamp('started_at')->nullable();
+            $table->timestamp('completed_at')->nullable();
+            $table->string('status')->default('not_started');
+            // Relations:
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('questionnaire_id')->constrained();
+            $table->unique(['user_id', 'questionnaire_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_questionnaire');
+    }
+};
