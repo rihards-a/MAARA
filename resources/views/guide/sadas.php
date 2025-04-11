8f36688f49ee9e109ff8ad7127d39b2ec10f1cfb
apelzoid <groove11@inbox.lv>
2025-04-11 14:29:08 +0300

    guide index redesigned

---
+++ resources/views/guide/sadas.php
@@ -0,0 +1,30 @@
+@extends('layouts.app_layout_with_navbar')
+
+@section('title', 'Pieejamais pabalsts')
+
+@section('main_content')
+<section class="welcome-section">
+    <h1 class="welcome-title">Pieejamais pabalsts</h1>
+
+    <div class="welcome-content">
+        <div class="welcome-text">
+            <p>
+            ​Kur un kā pieteikties apbedīšanas pabalstam, lai segtu tuvinieka bēru izdevumus – termiņi un saites.<br>
+            <br>
+            🔸Pēc tuvinieka aiziešanas ir iespējams saņemt finansiālu atbalstu apbedīšanas izdevumu segšanai. Latvijā šādu pabalstu piešķir <b>Valsts sociālās apdrošināšanas aģentūra (VSAA)</b>, un atsevišķos gadījumos arī pašvaldības.<br>
+            🔸 Pabalsts jāpieprasa 6 mēnešu laikā no miršanas dienas. Iesniegumu var iesniegt: portālā Latvija.gov.lv, klātienē jebkurā VSAA klientu apkalpošanas centrā, elektroniski, parakstot ar drošu elektronisko parakstu, pa pastu.<br>
+            Saite - https://www.vsaa.gov.lv/lv/pakalpojumi/apbedisanas-pabalsta-pieskirsana-un-izmaksasana <br>
+            <br>
+            🔸Dzīvības apdrošināšana: ja mirušajam bija dzīvības apdrošināšanas polise, tās ietvaros var būt paredzēta atlīdzība, kas palīdz segt bēru izdevumus. Šādā gadījumā jāsazinās ar attiecīgo apdrošināšanas sabiedrību, lai noskaidrotu atlīdzības saņemšanas kārtību.<br>
+            🔸Darba devēja vai arodbiedrības atbalsts: daži darba devēji vai arodbiedrības piedāvā finansiālu palīdzību darbinieka vai viņa ģimenes locekļa nāves gadījumā.<br>
+            🔸Pašvaldības pabalsti: atsevišķas pašvaldības var piedāvāt papildu pabalstus bēru izdevumu segšanai. Ieteicams sazināties ar mirušā deklarētās dzīvesvietas pašvaldības sociālo dienestu, lai noskaidrotu pieejamās iespējas.<br>
+            <br>
+            Detalizētāka informācija par iespējamo pabalstu tuvinieka nāves gadījumā - 
+            <a href="https://www.lm.gov.lv/lv/apbedisanas-pabalsts?utm_source=chatgpt.com&utm_source=https%3A%2F%2F" target="_blank" rel="noopener noreferrer">
+                Lm.gov.lv
+            </a>
+            </p>
+        </div>
+    </div>
+</section>
+@endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,33 +4,47 @@
 
 @section('main_content')<section class="welcome-section">
     <p class="welcome-text">
-    Gan komanda, kas veidojusi šo lapu, gan daudzi citi cilvēki Latvijā ir saskārušies ar apjukumu un neskaidrību par to, kas jādara, kad šo pasauli pamet tuvs cilvēks. Mūsu misija ir sniegt atbalstu šajā brīdī un nodrošināt, ka eksistē viens, uzticams avots, kas kalpo kā palīgs šajā brīdī, kad daudzi lēmumi jāpieņem pirmo reizi.<br>
-
-Neatbildēto jautājumu šādos gadījumos ir vairāk, ja aizgājējs nav atstājis norādījumus un informāciju, kas palīdzētu tuviniekiem pieņemt dažādus lēmumus un veikt nepieciešamās darbības. Tāpēc mēs aicinām Tevi iepazīties arī ar mūsu ceļvedi atbildīgajiem: lēmumi un informācija, kas laikus ir jānorāda katram no mums, lai mūsu tuvinieki pieredzētu mazāk novēršanu pārdzīvojumu grūtajā brīdī.<br>
-
-Visi materiāli šajā lapā ir pieejami bez maksas, bet mēs novērtēsim atsauksmes, ieteikumus un ziedojumus, lai varam veikt šo darbu vēl augstākā kvalitātē.
+    <h2>Atbildes uz jautājumiem par aiziešanu.</h2><br>
+    Mums katram dzīves laikā sanāk kaut reizi izdzīvot sēras, taču nāvi ne vienmēr ir jāpavada apjukumam. <br>
+    <br>
+    Mēs esam šeit, lai sniegtu Tev noderīgu informāciju par aiziešanas praktiskajiem aspektiem - gan, ja šobrīd esi atbildīgs par kāda cita izvadīšanu, gan, ja vēlies pārliecināties, ka Taviem tuviniekiem būs pieejama visa nepieciešamā informācija, lai spētu vieglāk tikt galā ar praktiskajiem aspektiem Tevis aiziešanas gadījumā.<br>
     </p>
   </section>
 
-  <div class="cards-container">
-    <div class="card">
-      <img src="{{ asset('images/image3.jpg') }}" alt="Ceļvedis tiem">
-      <div class="card-content">
-        <h2 class="card-title">Kas jādara pēc tuvinieka nāves: soli pa solim</h2>
-        <p class="card-text">Šis gids kalpos kā padomdevējs un asistents tiem, kas patlaban kārto praktiskus jautājumus, kas saistīti ar tuvinieka aiziešanu: sākot ar nāves iestāšanās brīdi, līdz pat gadam pēc nāves.</p>
-        <button class="card-button" data-url="/guide">Izvēlēties</button>
-      </div>
-    </div>
+      <div class="cards-container">
+        <div class="card vertical-card">
+          <img src="{{ asset('images/guide.png') }}" alt="Ceļvedis tiem" class="card-image-guide">
+            <div class="card-content-guide">
+              <div>
+                <h2 class="card-title">Kas jādara pēc tuvinieka nāves: soli pa solim</h2>
+                <p class="card-text">Šis bezmaksas ceļvedis kalpo kā padomdevējs un asistents tiem, kas patlaban kārto praktiskus jautājumus, kas saistīti ar tuvinieka aiziešanu: sākot jau ar nāves iestāšanās brīdi.</p>
+              </div>
+              <button class="card-button" data-url="/guide">Skatīt</button>
+            </div>
+        </div>
+ 
 
-    <div class="card">
-      <img src="{{ asset('images/image2.jpg') }}" alt="Ceļvedis ikkatram">
-      <div class="card-content">
-        <h2 class="card-title">Ceļvedis atbildīgajiem: </h2>
-        <p class="card-text">Šeit esam apkopojuši visus lēmumus, ko cilvēkam vajadzētu pieņemt saistībā ar savu "pēcnāvi" uz Zemes, lai atvieglotu šo smago brīdi tuviniekiem: sākot ar medicīniskām preferencēm un beidzot ar norādījumiem par izvadīšanu.</p>
-        <button class="card-button" data-url={{route('dashboard')}}>Izvēlēties</button>
+    <div class="card vertical-card">
+        <img src="{{ asset('images/planosanas_riks.png') }}" alt="Ceļvedis ikkatram" class="card-image">
+        <div class="card-content">
+          <div>
+            <h2 class="card-title">Pēcdzīves plānošanas rīks</h2>
+            <p class="card-text">Šovasar lietotāju rokās nodosim Latvijā pirmo ''pēcdzīves'' plānošanas rīku. Tā mērķis būs atvieglot darbu ikvienam, kurš vēlās nodrošināt, ka aiziešanas vai rīcībnespējas gadījumā, aiz sevis tiek atstāta skaidrība un sirdsmiers.<br>
+            <br>
+            MAARA sistēma Tevi ērti un droši izvedīs caur virkni jautājumu, sniegs noderīgu informāciju un ļaus saglabāt un apkopot savas izvēles: sākot ar medicīniskiem lēmumiem un pilnvarām, informāciju par Taviem īpašumiem digitālajā vidē, un beidzot ar bēru preferencēm un mājdzīvnieka kopšanas norādījumiem.<br>
+            <br>
+            <b>Mēs Tev paziņosim, kad plānošanas rīks būs gatavs!</b>
+            </p>
+          </div>
+          <div>
+            <div class="card-email">
+            <input type="email" placeholder="Ievadi savu epastu">
+            <button class="card-button-green" data-url={{route('dashboard')}}>Pieteikties</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
 
   <script>
     document.querySelectorAll(".card-button").forEach(button => {
