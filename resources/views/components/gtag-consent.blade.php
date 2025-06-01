946894815dd02f56f4815e596b62ccbbcb1657f6
Rihards <rihards.abolins1@gmail.com>
2025-06-01 14:57:44 +0300

    Modularized and unified the gtag consent js logic.

--- resources/views/components/gtag-consent.blade.php
+++ resources/views/components/gtag-consent.blade.php
@@ -18,25 +18,34 @@
   gtag('js', new Date());
   
   document.addEventListener('DOMContentLoaded', function() {
+    const consentKey = 'gtag_consent_given';
     const consentButton = document.getElementById('accept-cookies');
-    if (localStorage.getItem('gtag_consent_given') === 'true') {
+    const cookieBanner = document.getElementById('cookie-consent-banner');
+
+    if (localStorage.getItem(consentKey) === 'true') {
         give_consent();
     } else if (consentButton) {
       consentButton.addEventListener('click', function() {
-        give_consent();
+        setTimeout(() => { // slide in the banner
+          cookieBanner?.classList.remove('translate-y-full');
+        }, 500);
+        consentButton.addEventListener('click', give_consent);
       });
     }
-  });
 
-  function give_consent() {
-    gtag('consent', 'update', {
-      'ad_storage': 'granted',
-      'ad_user_data': 'granted',
-      'ad_personalization': 'granted',
-      'analytics_storage': 'granted',
-    });
-    gtag('config', '{{ $trackingId }}'); // configurable things here: https://developers.google.com/analytics/devguides/collection/ga4/reference/config
-    localStorage.setItem('gtag_consent_given', 'true');
-    document.getElementById('cookie-consent-banner')?.remove();
-  }
+    function give_consent() {
+      gtag('consent', 'update', {
+        'ad_storage': 'granted',
+        'ad_user_data': 'granted',
+        'ad_personalization': 'granted',
+        'analytics_storage': 'granted',
+      });
+      gtag('config', '{{ $trackingId }}'); // configurable things here: https://developers.google.com/analytics/devguides/collection/ga4/reference/config
+      localStorage.setItem(consentKey, 'true');
+      cookieBanner?.classList.add('translate-y-full'); // remove the banner with a sliding transition
+      setTimeout(() => {
+        cookieBanner?.remove();
+      }, 500);
+    }
+  });
 </script>
