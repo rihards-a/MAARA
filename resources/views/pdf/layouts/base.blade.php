ccfc50bd14200a076f596581f6acf2b98655537a
Rihards <rihards.abolins1@gmail.com>
2025-06-21 22:46:09 +0300

    Set page sizes that may need to be changed in the future. Added CSS that should prevent the most basic of text spilling in the following page.

--- resources/views/pdf/layouts/base.blade.php
+++ resources/views/pdf/layouts/base.blade.php
@@ -16,9 +16,21 @@
             padding: 0;
         }
         
-        /* Ensure all text elements inherit the font */
+        {{-- Ensure all text elements inherit the font --}}
         h1, h2, h3, h4, h5, h6, p, div, span, td, th, li {
             font-family: inherit;
+        }
+
+        
+        {{-- For preventing single lines or words from spilling in to the next page --}}
+        @page {
+            margin: 50px 50px 100px 50px;
+        }
+
+        p, div, li, table, tr {
+            page-break-inside: avoid;
+            orphans: 3;
+            widows: 3;
         }
     </style>
 </head>
